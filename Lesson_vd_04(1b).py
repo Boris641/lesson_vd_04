3d042bf8f0ef3292020771a6465f04d01ad4193d
Boris641 <163911643+Boris641@users.noreply.github.com>
2024-06-06 20:41:09 +0400

    lesson_04_vd

---
+++ Lesson_vd_04(1b).py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/article/")
+def article():
+    return render_template("article.html")
+
+@app.route("/contacts/")
+def contacts():
+    return render_template("contacts.html")
+
+
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+

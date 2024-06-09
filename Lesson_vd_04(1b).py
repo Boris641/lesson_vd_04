10a4701a42dfb9b414e3e3cd87d31c409057d449
Boris641 <163911643+Boris641@users.noreply.github.com>
2024-06-09 20:17:49 +0400

    Update Lesson_vd_04(1b).py

--- Lesson_vd_04(1b).py
+++ Lesson_vd_04(1b).py
@@ -15,29 +15,7 @@ def article():
 @app.route("/contacts/")
 def contacts():
     return render_template("contacts.html")
-=======
-def hello_word():
 
-    return render_template("navigation.html")
-
-@app.route("/home/")
-def home():
-
-    return render_template("home.html")
-
-
-
-@app.route("/article/")
-def article():
-
-    return render_template("article.html")
-
-@app.route("/сontacts/")
-def сontacts():
-
-    return render_template("contact.html")
-
->>>>>>> origin/main
 
 
 
